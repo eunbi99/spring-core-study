60671f499ebe831c5e900cdf1a9178a968636fa3
eunbi99 <silverbi99@naver.com>
2021-07-29 13:52:31 +0900

    Chapter 4. 컴포넌트 스캔 및 필터

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+//자동으로 스프링 빈으로 등록 (excludeFilters : 컴포넌트 스캔 대상에서 제외 -> 기존 예제인 AppConfig가 다시 등록 되면 충돌나기 때문에 )
+@ComponentScan(
+        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes=Configuration.class))
+
+public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService  {
 
     //더이상 MemberServiceImpl은 MemoryMemberRepository를 의존하지 않는다.
     private final MemberRepository memberRepository; //단지 MemberRepository 인터페이스만 의존!
 
-    //생성자를 통해 어떤 구현 객체가 주입될지 알 수 없다. (AppConfig에서 결정됨)
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
