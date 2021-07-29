b13773d16573fed04aff5540c6f00f0d4dbd9bab
eunbi99 <silverbi99@naver.com>
2021-07-29 16:38:34 +0900

    Chapter 5. 의존관계 자동 주입 방법

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,37 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean{
+        @Autowired(required = false) //메서드 자체가 호출 x
+        public void setNoBean1(Member noBean1){
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2){
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean(Optional<Member> noBean3){
+            System.out.println("noBean3 = " + noBean3);
+        }
+
+    }
+
+}
