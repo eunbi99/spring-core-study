9f1bee0da172e4316653c2e4ec94759ccf992dc8
eunbi99 <silverbi99@naver.com>
2021-07-30 18:08:23 +0900

    Chapter 5. 롬복과 의존성 주입 애너테이션

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,6 +2,12 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component // 똑같은 타입의 스프링 빈이 중복될때 오류
+@Primary
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount =1000; //1000원 할인
